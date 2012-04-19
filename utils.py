ab641a45f0b73a6cdf34d7db42af80ba1b5caefc
Laurent Perrinet <orez.org@gmail.com>
2012-04-19 16:24:49 +0200

    making a nice tiling of the XYUV space
    I used an hexagonal tiling of space which seems to optimize packing + a
    log-polar organization of velocity preference / fixed random to get
    normally distributed velocities / still some decisions to make if we
    want to gather parameters somewhere…

--- utils.py
+++ utils.py
@@ -1,28 +1,69 @@
-import numpy
+import numpy as np
 import numpy.random as rnd
 
 
-def set_tuning_prop(n_cells):
+def set_tuning_prop(n_cells, mode='hexgrid', v_max=2.0):
     """
-    Place n_cells in a 4-dimensional space by random.
+    Place n_cells in a 4-dimensional space by some mode (random, hexgrid, ...).
     The position of each cell represents its excitability to a given a 4-dim stimulus.
+    The radius of their receptive field is assumed to be constant (TODO: one coud think that it would depend on the density of neurons?)
 
     return value:
-        tp = numpy.array((n_cells, 4))
+        tp = set_tuning_prop(n_cells)
         tp[:, 0] : x-position
         tp[:, 1] : y-position
         tp[:, 2] : u-position (speed in x-direction)
         tp[:, 3] : v-position (speed in y-direction)
 
-        All values are in range [0..1]
+        All x-y values are in range [0..1]
+
     """
-    tuning_prop = numpy.zeros((n_cells, 4))
-    for cell in xrange(n_cells):
-        tuning_prop[cell, 0] = rnd.rand()
-        tuning_prop[cell, 1] = rnd.rand()
-        tuning_prop[cell, 2] = rnd.rand()
-        tuning_prop[cell, 3] = rnd.rand()
+    tuning_prop = np.zeros((n_cells, 4))
+    if mode=='random':
+        for cell in xrange(n_cells):
+            tuning_prop[cell, 0] = rnd.rand()
+            tuning_prop[cell, 1] = rnd.rand()
+            tuning_prop[cell, 2] = v_max * rnd.randn()
+            tuning_prop[cell, 3] = v_max * rnd.randn()
+    elif mode=='hexgrid':
+        N_V, N_theta = 8, 16 # resolution in velocity norm and direction
+        log_scale = 1. # base of the logarithmic tiling of particle_grid; linear if equal to one
+
+        v_rho = np.logspace(np.log(v_max/N_V)/np.log(log_scale),
+                            np.log(v_max)/np.log(log_scale), num=N_V,
+                            endpoint=True, base=log_scale)
+        v_theta = np.linspace(0, 2*np.pi, N_theta, endpoint=False)
+        parity = np.arange(N_V) % 2
+
+        N_RF = np.int(n_cells/N_V)
+        # np.sqrt(np.sqrt(3)) comes from resolving the problem "how to quantize the square with a hex grid of N_RF**2 dots?"
+        N_RF_X = np.int(np.sqrt(N_RF*np.sqrt(3)))
+        N_RF_Y = np.int(np.sqrt(N_RF/np.sqrt(3)))
+        RF = np.zeros((2, N_RF_X*N_RF_Y))
+        X, Y = np.mgrid[0:1:1j*(N_RF_X+1), 0:1:1j*(N_RF_Y+1)]
+    
+        # It's a torus, so we remove the first row and column to avoid redundancy (would in principle not harm)
+        X, Y = X[1:, 1:], Y[1:, 1:]
+        # Add to every even Y a half RF width to generate hex grid
+        Y[::2, :] += (Y[0, 0] - Y[0, 1])/2 # 1./N_RF
+        RF[0, :] = X.ravel()
+        RF[1, :] = Y.ravel()
+    
+        # wrapping up:
+        assert N_V * N_theta * N_RF_X*N_RF_Y==n_cells
+
+        index = 0
+        for i_v_rho, rho in enumerate(v_rho):
+            for i_theta, theta in enumerate(v_theta):
+                for i_RF in range(N_RF_X*N_RF_Y):
+                    tuning_prop[index, 0] = RF[0, i_RF]
+                    tuning_prop[index, 1] = RF[1, i_RF]
+                    tuning_prop[index, 2] = np.cos(theta + parity[i_v_rho] * np.pi / N_theta) * rho
+                    tuning_prop[index, 3] = np.sin(theta + parity[i_v_rho] * np.pi / N_theta) * rho
+                    index += 1
+         
     return tuning_prop
+
 
 
 def get_input(tuning_prop, time_steps, motion):
@@ -40,7 +81,7 @@ def get_input(tuning_prop, time_steps, motion):
         motion: type of motion, maybe filename to movie, ... ???
     """
     n_cells = tuning_prop[:, 0].size
-    L = numpy.zeros((n_cells, time_steps)) # maybe use sparse matrices or adjacency lists instead
+    L = np.zeros((n_cells, time_steps)) # maybe use sparse matrices or adjacency lists instead
 
     # compute the motion energy input to all cells
     """
@@ -78,7 +119,7 @@ def convert_motion_energy_to_spike_trains(input_vecs, dt=1, tgt_fn_base='input_s
                 st.append(i * dt)
 
         output_fn = tgt_fn_base + str(cell) + '.dat'
-        numpy.savetxt(output_fn, numpy.array(st)) # to be changed to binary numpy.save
+        np.savetxt(output_fn, numpy.array(st)) # to be changed to binary numpy.save
 
 
 
