1d3753ed1a2fdcbfb6c4a1b3804da57522d5620a
Laurent Perrinet <orez.org@gmail.com>
2012-11-06 11:19:01 +0100

    added NCPUS parameter
    …. doh ...

--- run_all.sh
+++ run_all.sh
@@ -1,10 +1,12 @@
-echo 'Starting at' `date`
+export NCPUS=8
+export NCPUS=1
+echo 'Starting at ' `date` ' with ' $NCPUS ' cpus'
 python prepare_tuning_prop.py
 python prepare_selective_inhibition.py
-mpirun -np 8 python prepare_spike_trains.py
+mpirun -np $NCPUS python prepare_spike_trains.py
 echo 'Preparation stopped at' `date`
-#mpirun -np 8 python prepare_connections.py
-#mpirun -np 8 python NetworkSimModuleNoColumns.py
+#mpirun -np $NCPUS python prepare_connections.py
+#mpirun -np $NCPUS python NetworkSimModuleNoColumns.py
 python NetworkSimModuleNoColumns.py
 python analyse_simple.py
 python analyse_input.py
