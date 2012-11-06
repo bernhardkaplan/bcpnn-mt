e6fcafd427e78588f6ee8264d97b0f540b7ce932
Laurent Perrinet <orez.org@gmail.com>
2012-11-06 17:35:27 +0100

    switching to nest-2.0.0

--- run_all.sh
+++ run_all.sh
@@ -1,13 +1,13 @@
 export NCPUS=8
 export NCPUS=1
 echo 'Starting at ' `date` ' with ' $NCPUS ' cpus'
-python prepare_tuning_prop.py
+python prepare_tuning_prop.py 0.1 0.3 # give blur_X and blur_V as arguments
 python prepare_selective_inhibition.py
-mpirun -np $NCPUS python prepare_spike_trains.py
+mpirun -np $NCPUS python prepare_spike_trains.py 0.1 0.3 
+mpirun -np $NCPUS python prepare_connections.py
 echo 'Preparation stopped at' `date`
-#mpirun -np $NCPUS python prepare_connections.py
-#mpirun -np $NCPUS python NetworkSimModuleNoColumns.py
-python NetworkSimModuleNoColumns.py
+mpirun -np $NCPUS python NetworkSimModuleNoColumns.py
+echo 'Simulation stopped at' `date`
 python analyse_simple.py
 python analyse_input.py
 #python plot_connlist_as_colormap.py
