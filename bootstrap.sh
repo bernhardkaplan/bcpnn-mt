62815282a290fd346112a2015bd436d097508c19
Laurent Perrinet <laurent.perrinet@gmail.com>
2013-03-20 16:38:50 +0100

    bootstrap script to launch from an empty debain

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+# installing requirements on an empty machine
+sudo aptitude update 
+sudo aptitude upgrade 
+sudo aptitude install python-pynn
+sudo aptitude install python-matplotlib
--- run_all.sh
+++ run_all.sh
@@ -1,10 +1,12 @@
 echo 'Starting at' `date`
 python prepare_tuning_prop.py
 python prepare_selective_inhibition.py
-mpirun -np 8 python prepare_spike_trains.py
+#mpirun -np 8 python prepare_spike_trains.py
+python prepare_spike_trains.py
 echo 'Preparation stopped at' `date`
 #mpirun -np 8 python prepare_connections.py
-mpirun -np 8 python NetworkSimModuleNoColumns.py
+#mpirun -np 8 python NetworkSimModuleNoColumns.py
+python NetworkSimModuleNoColumns.py
 #python NetworkSimModuleNoColumns.py
 python analyse_simple.py
 python analyse_input.py
