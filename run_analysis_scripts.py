b55279b011a293dee482d51d4865dc18c9f4dfe4
Bernhard Kaplan <Bernhard.Kaplan@gmail.com>
2014-01-10 17:20:11 +0100

    CHANGE: run_analysis_scripts.py

--- run_analysis_scripts.py
+++ run_analysis_scripts.py
@@ -12,10 +12,20 @@ f = file(param_fn, 'r')
 print 'Loading parameters from', param_fn
 params = json.load(f)
 
-script_names = ['plot_rasterplots.py', 'plot_prediction.py', 'plot_connectivity_profile.py', 'plot_weight_and_delay_histogram.py ee',\
-        'plot_weight_and_delay_histogram.py ei', 'plot_weight_and_delay_histogram.py ie', 'plot_weight_and_delay_histogram.py ii']
+#script_names = ['plot_rasterplots.py', 'plot_prediction.py', 'plot_connectivity_profile.py', 'plot_weight_and_delay_histogram.py ee',\
+#        'plot_weight_and_delay_histogram.py ei', 'plot_weight_and_delay_histogram.py ie', 'plot_weight_and_delay_histogram.py ii']
+#for sn in script_names:
+#    os.system('python %s %s' % (sn, params['folder_name']))
 
+import plot_prediction as pp
 
-for sn in script_names:
-    os.system('python %s %s' % (sn, params['folder_name']))
-
+if params['n_grid_dimensions'] == 2:
+    pp.plot_prediction_2D(params)
+else:
+    pp.plot_prediction_1D(params)
+os.system('python plot_rasterplots.py %s' % params['folder_name'])
+os.system('python plot_weight_and_delay_histogram.py %s' % params['folder_name'])
+os.system('python plot_connectivity_profile.py %s' % params['folder_name'])
+os.system('python PlottingScripts/PlotAnticipation.py %s' % params['folder_name'])
+os.system('python PlottingScripts/plot_contour_connectivity.py %s' % params['folder_name'])
+os.system('ristretto %s' % (params['figures_folder']))
