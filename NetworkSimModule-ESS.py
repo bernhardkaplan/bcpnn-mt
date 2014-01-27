af877d9849fa98cf7ed470dcd3d5cfa41f71ec70
bernhard <bernhard.kaplan@gmail.com>
2014-01-27 17:20:38 +0100

    import hardware.brainscales

--- NetworkSimModule-ESS.py
+++ NetworkSimModule-ESS.py
@@ -18,7 +18,7 @@ import CreateConnections as CC
 import utils
 import simulation_parameters
 
-exec("from pyNN.nest import *")
+import pyNN.hardware.brainscales as sim
 import pyNN
 import pyNN.space as space
 from pyNN.utility import Timer # for measuring the times to connect etc.
