2c682f6b172a7428e2536c57b25249c43dfb5e03
Bernhard Kaplan <bernhard.kaplan@gmail.com>
2015-03-05 01:11:11 +0100

    midnight nonsense

--- mean_field_model.py
+++ mean_field_model.py
@@ -14,13 +14,27 @@ def skew_normal(x, mu, sigma, alpha):
 
 if __name__ == '__main__':
 
-
-    t_axis = np.arange(0., 1., 0.1)
-    x_axis = np.arange(0., 1., 0.02)
-    sigma = 0.1
+    dx = 0.02
+    dt = 0.05
+    x_max = 3.
+    t_axis = np.arange(0., .2, dt)
+    x_axis = np.arange(0., x_max, dx)
+    n_x = x_axis.size
+    n_time = t_axis.size
+    activity_field = np.zeros(n_x)
+    activity_field_prv = np.zeros(n_x)
+    sigma_conn = 0.05   # width connectivity kernel
     alpha = 0.5
     x0, v0 = 0.2, 1.
-    blur_x = 0.1
+    blur_x = 0.1    # stimulus width
+    tau = .3
+    w_ee_amp = 0.005
+    w_ie_amp = 0.001
+    w_input = 0.
+    t_start_blank = 0.5
+    t_stop_blank = 0.8
+    E_max = 1.
+    E_min = -1.
 
     clim = (t_axis[0], t_axis[-1])
     norm = matplotlib.colors.Normalize(vmin=clim[0], vmax=clim[1])
@@ -30,15 +44,33 @@ if __name__ == '__main__':
 
     fig = pylab.figure()
     ax = fig.add_subplot(111)
-
     for i_time, t_ in enumerate(t_axis):
         x_stim = x0 + v0 * t_
-        stimulus = mlab.normpdf(x_axis, x_stim, blur_x)
-        ax.plot(x_axis, stimulus, c=colorlist[i_time])
-#        for i_x, x_ in enumerate(x_axis):
+        print 't_:', t_
+        if t_ > t_start_blank and t_ < t_stop_blank:
+            stimulus = np.zeros(n_x)
+            print 'stim = 0 at t=%.2f' % t_
+        else:
+            stimulus = mlab.normpdf(x_axis, x_stim, blur_x)
+        ax.plot(x_axis, stimulus, c=colorlist[i_time], ls=':')
+        for i_x, x_ in enumerate(activity_field):
+            # network contribution
+            net_input = 0.
+            for j_x, x_src in enumerate(activity_field_prv):
+#                net_input += w_ee_amp * mlab.normpdf(x_axis, x_, sigma_conn)[j_x] * (activity_field_prv[i_x] - E_max) - w_ie_amp * (activity_field_prv[i_x] - E_min)
+                net_input += w_ee_amp * mlab.normpdf(x_axis, x_src, sigma_conn)[j_x] #- w_ie_amp
+#                print 'net_input:', net_input
+            da = - tau * activity_field_prv[i_x] * dt + w_input * stimulus[i_x] + net_input
+            activity_field[i_x] += da
+            activity_field_prv[i_x] = activity_field[i_x]
+        ax.plot(x_axis, activity_field, c=colorlist[i_time], label='t=%.2f' % t_)
+
+#            update(activity_field, activity
+
+
 
             #activation = skew_normal(
-
+    ax.legend()
     pylab.show()
 #ani = animation.FuncAnimation(fig, run, data_gen, blit=True, interval=10,
 #    repeat=False)
