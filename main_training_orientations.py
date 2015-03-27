5616ed73c3b3581d02c84d42ed4ce0a7e6dbc8af
Bernhard Kaplan <bernhard.kaplan@gmail.com>
2015-03-27 11:37:04 +0100

    buggy milner training

--- main_training_orientations.py
+++ main_training_orientations.py
@@ -43,12 +43,15 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         params['taui_bcpnn'] = float(sys.argv[1])
         params['bcpnn_params']['tau_i'] = params['taui_bcpnn']
-        folder_name = 'TrainingSim_%s_%dx%dx%d_%d-%d_taui%d_nHC%d_nMC%d_vmin%.1f_vmax%.1f/' % ( \
+        if params['symmetric_tauij']:
+            params['tauj_bcpnn'] = params['taui_bcpnn']
+            params['bcpnn_params']['tau_j'] = params['tauj_bcpnn']
+        folder_name = 'TrainingSim_%s_%dx%dx%d_%d-%d_taui%d_nHC%d_nMC%d_vstim%.1f/' % ( \
                 params['sim_id'], params['n_training_cycles'], params['n_training_v'], params['n_training_x'], \
                 params['stim_range'][0], params['stim_range'][1], \
                 params['bcpnn_params']['tau_i'], \
                 params['n_hc'], params['n_mc_per_hc'], \
-                params['v_min_tp'], params['v_max_tp'])
+                params['v_stim_training'])
         GP.set_filenames(folder_name=folder_name)
 
 #    trained_stimuli = []
